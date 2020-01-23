52541aa0d018b33145756f946775b635f7ec65c5
d-yandrapu <55582117+d-yandrapu@users.noreply.github.com>
2020-01-23 16:14:48 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,15 +4,18 @@ variable "instance_count" {
 
 variable "key_name" {
   description = "Private key name to use with instance"
+  type = "string"
   default     = "Packer"
 }
 
 variable "instance_type" {
   description = "AWS instance type"
+  type = "string"
   default     = "t2.micro"
 }
 
 variable "ami" {
   description = "LIUNX to launch the instances"
+  type = "string"
   default = "ami-062f7200baf2fa504"
 }
