2c2e2ea13d10e40b94d8a3a1a98ed53888921c24
d-yandrapu <55582117+d-yandrapu@users.noreply.github.com>
2020-01-23 16:23:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+# Terraform state will be stored in S3
+terraform {
+  backend "s3" {
+    bucket = "terraform-bucket-durga"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
 # Use AWS Terraform provider
 provider "aws" {
   region = "us-east-1"
